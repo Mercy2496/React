7fa2abe9038c857192e2d43de24040606f85ff22
Mercy2496 <njengammercy@gmail.com>
2024-07-01 17:13:33 +0300

    useState

--- ReactBasics/src/App.jsx
+++ ReactBasics/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Employee from './components/employee'
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("dev");
 
   return (
     <>
       <div>
+        <input type="text" onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }} />
         <Employee name="Jude" role="staff" />
-        <Employee name="Kate" />
-        <Employee />
+        <Employee name="Kate" role={role}/>
+        <Employee name="dave"/>
       </div>
     </>
   )
--- ReactBasics/src/components/employee.jsx
+++ ReactBasics/src/components/employee.jsx
@@ -1,10 +1,12 @@
 function Employee(props) {
+    
     return (
-        <>
-        <h3>Employee {props.name}</h3>
-        <p>{props.role ? props.role : "Role Unknown"}</p>
-        </>
-    )
+            <>
+            <h3>Employee: {props.name}</h3>
+            <p>{props.role ? props.role : "Role Unknown"}</p>
+            
+            </>
+        )
 }
 
 export default Employee;
