c4695cee108a8db916bea06e4eb984bfee5719ef
Mercy2496 <njengammercy@gmail.com>
2024-07-01 15:34:54 +0300

    props

---
+++ ReactBasics/src/components/employee.jsx
@@ -0,0 +1,10 @@
+function Employee(props) {
+    return (
+        <>
+        <h3>Employee {props.name}</h3>
+        <p>{props.role ? props.role : "Role Unknown"}</p>
+        </>
+    )
+}
+
+export default Employee;
